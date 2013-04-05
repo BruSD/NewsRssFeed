4c4f1078b5684ae72d362d06250e46ad2457a03f
andrew tivodar <andrewtivodar@yahoo.com>
2013-04-05 16:54:47 +0300

    Add podcast detail activity

---
+++ src/com/newsrss/Feed/PodcastDetailView.java
@@ -0,0 +1,11 @@
+package com.newsrss.Feed;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AndrewTivodar
+ * Date: 05.04.13
+ * Time: 16:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class PodcastDetailView {
+}
